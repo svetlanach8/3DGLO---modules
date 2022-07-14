30eb67da043453e36cf3cd83c57311455b90756a
Svetlana <svetlana-chistjakova8@rambler.ru>
2022-07-14 23:30:18 +0300

    delete_module_19_21

--- lesson21/src/index.js
+++
@@ -1,9 +0,0 @@
-import timer from "./modules/timer";
-import menu from "./modules/menu";
-import modal from "./modules/modal";
-import validation from "./modules/validation";
-
-timer('22 July 2022');
-menu();
-modal();
-validation();
