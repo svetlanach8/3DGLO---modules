6e7f3d0203d171190c3c2d501b64f40617f3bf3c
Svetlana <svetlana-chistjakova8@rambler.ru>
2022-07-14 23:40:36 +0300

    add_homework_22

---
+++ lesson22_dop/src/index.js
@@ -0,0 +1,11 @@
+import timer from "./modules/timer";
+import menu from "./modules/menu";
+import modal from "./modules/modal";
+import validation from "./modules/validation";
+import tabs from "./modules/tabs";
+
+timer('22 July 2022');
+menu();
+modal();
+validation();
+tabs();
